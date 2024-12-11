fb4cc15c668d2ac64c541b7fa651ab76015d7705
Profesor C1 <admin@PROF-C1.local>
2024-12-11 20:54:32 +0200

    curs1

---
+++ curs1/Aplicatii_curs1/js/ap1.js
@@ -0,0 +1,6 @@
+///alert('Salut, JavaScript!');
+// console.log('Salut JavaScript');
+// window.alert('Salut JavaScript');
+// confirm('Salut JavaScript');
+
+console.log([1, 2, 3, 4]);
---
+++ curs1/Aplicatii_curs1/js/ap4.js
@@ -0,0 +1,3 @@
+alert("Acest mesaj a fost apelat!");
+document.write("<h2>Mesaj afisat din JavaScript</h2>");
+document.getElementById("mydemo").innerText = 'Continut demo pentru paragraful cu ID = mydemo';
