08f03968a693cd576dc0ae48289ccdca789b7662
Profesor C1 <admin@PROF-C1.local>
2025-01-16 21:03:15 +0200

    curs5

--- curs5/Aplicatii_curs5/js/app.js
+++ curs5/Aplicatii_curs5/js/app.js
@@ -1,18 +1,20 @@
 
 console.log(movies);
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 const main = document.getElementById("main");
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 showMovies(movies, searchTerm='');
 function showMovies(results, searchTerm=''){
-if(searchTerm !==''){
-    alert(searchTerm);
-    results = results.filter(function (element) {
-        return (
-            element.title.toLowerCase().indexOf(
-                searchTerm) > -1);
-        });
-}
+    if(searchTerm !==''){
+        alert(searchTerm);
+        results = results.filter(function (element) {
+            // return element.title == searchTerm;
+            return (element.title.toLowerCase().indexOf(searchTerm) > -1);
+          });
+    }
     results.forEach(element => {
         const el = document.createElement('div');
         const image = document.createElement('img');
@@ -23,19 +25,19 @@ if(searchTerm !==''){
         el.appendChild(image);
         el.appendChild(text);
         main.appendChild(el);
-    });
-
+    }); 
 }
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     main.innerHTML = '';
+     
     const searchTerm = search.value;
+
     if (searchTerm) {
-        showMovies(movies, 
-            searchTerm.toLowerCase());
+        showMovies(movies, searchTerm.toLowerCase());
         search.value = "";
     }
 });
-
 
